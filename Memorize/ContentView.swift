1866261881a0166c25d497ed9be1698f3f3485e0
SkydevilK <skydevilk@gmail.com>
2021-08-22 23:10:25 +0900

    [Feat] Lecture 1

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by SkydevilK on 2021/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by SkydevilK on 2021/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
