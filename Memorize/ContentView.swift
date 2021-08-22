c93f4e7918119e9b251a9a19b45b63726e8f4eb9
SkydevilK <skydevilk@gmail.com>
2021-08-23 00:03:38 +0900

    [Feat] Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis =
+        ["🏀", "🥎", "🏐", "⚽️", "⛳️", "🥊", "🏑", "🪁",
+         "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚒",
+         "⌚️", "📱", "📲", "💻", "⌨️", "🖥", "🖨", "🚧"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, world!")
+        VStack {
+            ScrollView {
+                // 지연 수평 그리드 뷰(필요할 때 까지 항목을 생성 안함)
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add: some View {
+        Button{
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    
+    // State는 Databinding용 값이 수정되면 Destroyed 하고 다시 Recreate 함
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }else {
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
